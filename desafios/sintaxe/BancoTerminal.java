75a4d88430781d0334dc2ae29e5c859cb9adbfe9
Kelvin Medina <medinadev44@gmail.com>
2025-07-02 22:06:28 -0300

    Adiciona solução para o desafio BancoTerminal, arquivo corrigido

--- desafios/sintaxe/BancoTerminal.java
+++ desafios/sintaxe/BancoTerminal.java
@@ -1,8 +1,21 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         var sc = new Scanner(System.in);
+        System.out.println("Insira o número da agência: ");
+        var account =  sc.nextLine();
+        System.out.println("Digite o número da conta: ");
+        var numberAccount = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Digite o seu nome:");
+        var name = sc.nextLine();
+        System.out.println("Digite o saldo: ");
+        var balance = sc.nextDouble();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo R$%s já está disponível para saque", name, account, numberAccount, balance);
+
 
     }
 }
